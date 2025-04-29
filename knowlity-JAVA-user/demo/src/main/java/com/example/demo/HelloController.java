bcc0d8a943b27c85f4a9f2ca40fc596dd5d8300d
ahmed-kasdaoui <ahmed.kasdaoui@esprit.tn>
2025-04-29 10:45:23 +0100

    googleauth1

--- knowlity-JAVA-user/demo/src/main/java/com/example/demo/HelloController.java
+++ knowlity-JAVA-user/demo/src/main/java/com/example/demo/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
+    protected void onHelloButtonClick1() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
 }
